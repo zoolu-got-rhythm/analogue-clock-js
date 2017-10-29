0e234ab192c4d21275581161d232ce8e2bac2166
christopher morris <slime@christophers-MacBook-Pro.local>
2017-10-29 19:31:48 +0000

    analog clock visual functional

--- app.js
+++ app.js
@@ -9,12 +9,30 @@ var ctx = c.getContext("2d");
 var trail = [];
 var colours = [];
 
-function clockHand(hour){
+function clockHand(timeValue, timeType){
 
-        var secHandLength = (c.width / 3);
 
-        var angle = (hour - 15) * ((Math.PI * 2)) / 60;       // THE ANGLE TO MARK.
+        var secHandLength;
+        if(timeType === "secs"){
+            secHandLength = (c.width / 2);
+            ctx.lineWidth = 1;
+        }
 
+        if(timeType === "mins"){
+            secHandLength = (c.width / 2.2);
+            ctx.lineWidth = 5;
+
+        }
+
+        if(timeType === "hours"){
+            secHandLength = (c.width / 3);
+            ctx.lineWidth = 7;
+
+        }
+
+        var angle = timeType === "secs" || timeType === "mins" ?
+            (timeValue - 15) * ((Math.PI * 2)) / 60 :
+            (timeValue - 3) * ((Math.PI * 2)) / 12// THE ANGLE TO MARK.
 
         var x1 = (c.width / 2);
         var y1 = (c.height / 2);
@@ -23,7 +41,8 @@ function clockHand(hour){
 
         trail.push({x: x2, y: y2});
 
-        ctx.lineWidth = 1;            // HAND WIDTH.
+
+                   // HAND WIDTH.
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
@@ -41,27 +60,73 @@ var tick = function(){
     ctx.clearRect(0, 0, c.width, c.height);
     var time = new Date();
     console.log(time.getSeconds());
-    clockHand(i);
+    drawCircle();
+    drawMinTimeMarks(); 
+    drawHourTimeMarks();
+    clockHand(time.getSeconds(), "secs");
+    clockHand(time.getMinutes(), "mins");
+    clockHand(time.getHours(), "hours");
     i++;
     if(i >= 120)
         i = 0;
-    if(trail.length > 25){
-        trail.shift();
-    }
 
-    trail.forEach(function(ele, i){
-        ctx.lineWidth = i;            // HAND WIDTH.
-        ctx.beginPath();
-        // ctx.moveTo(ele.x, ele.y);
-        // ctx.lineTo(Math.random() + ele.x, Math.random() + ele.y);
 
-        ctx.strokeRect(ele.x, ele.y ,Math.random() + ele.x, Math.random() + ele.y);
 
-        ctx.strokeStyle = getRandomColor();
-        ctx.stroke();
-    });
     window.requestAnimationFrame(tick);
 };
+
+function drawHourTimeMarks(){
+    var mark = c.width / 2;
+
+    for(var i = 0; i < 12; i++){
+        var angle = (i - 3) * (Math.PI * 2) / 12;
+
+        ctx.lineWidth = 3;            // HAND WIDTH.
+        ctx.beginPath();
+
+        var x1 = (c.width / 2) + Math.cos(angle) * (mark);
+        var y1 = (c.height / 2) + Math.sin(angle) * (mark);
+        var x2 = (c.width / 2) + Math.cos(angle) * (mark - (mark / 10));
+        var y2 = (c.height / 2) + Math.sin(angle) * (mark - (mark / 10));
+
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+
+        ctx.strokeStyle = '#466B76';
+        ctx.stroke();
+
+    }
+}
+
+function drawMinTimeMarks(){
+
+    var mark = c.width / 2;
+
+    for(var i = 0; i < 60; i++){
+        var angle = (i - 15) * (Math.PI * 2) / 60;
+
+        ctx.lineWidth = 1;            // HAND WIDTH.
+        ctx.beginPath();
+
+        var x1 = (c.width / 2) + Math.cos(angle) * (mark);
+        var y1 = (c.height / 2) + Math.sin(angle) * (mark);
+        var x2 = (c.width / 2) + Math.cos(angle) * (mark - (mark / 15));
+        var y2 = (c.height / 2) + Math.sin(angle) * (mark - (mark / 15));
+
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+
+        ctx.strokeStyle = '#466B76';
+        ctx.stroke();
+
+    }
+}
+
+function drawCircle(){
+    ctx.beginPath();
+    ctx.arc(c.width / 2, c.height / 2, c.width / 2,0,2*Math.PI);
+    ctx.stroke();
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
